ceb2bb85416b3fc4407125fcc0d0c68b612e69b8
Ialzouby <85916427+Ialzouby@users.noreply.github.com>
2023-11-29 16:06:40 -0500

    FPV

---
+++ itis3135/scripts/script.js
@@ -0,0 +1,7 @@
+// script.js
+document.querySelectorAll('.gallery-item').forEach(item => {
+    item.addEventListener('click', () => {
+        const imgSrc = item.querySelector('img').getAttribute('src');
+        window.open(imgSrc, '_blank');
+    });
+});
