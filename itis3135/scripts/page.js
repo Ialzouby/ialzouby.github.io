a1b02c297c0275e6cc8060e31a2586778e86bd97
Ialzouby <85916427+Ialzouby@users.noreply.github.com>
2023-11-29 18:28:08 -0500

    CleanUp

--- itis3135/scripts/page.js
+++ itis3135/scripts/page.js
@@ -1,51 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Show the home section when the page loads
+   
     document.getElementById('home').style.display = 'block';
   
-    // Get all the navigation links
+   
     const navLinks = document.querySelectorAll('nav ul li a');
   
-    // Add click event listeners to each navigation link
+ 
     navLinks.forEach(function (link) {
         link.addEventListener('click', function (event) {
             event.preventDefault();
   
-            // Get the target section based on the href attribute of the link
             const targetId = link.getAttribute('href').substr(1);
             const targetSection = document.getElementById(targetId);
   
-            // Hide all sections
+           
             document.querySelectorAll('section').forEach(function (section) {
                 section.style.display = 'none';
             });
   
-            // Show the target section
             targetSection.style.display = 'block';
         });
     });
   });
   document.addEventListener('DOMContentLoaded', function () {
-    // Show the home section when the page loads
+    
     document.getElementById('home').style.display = 'block';
   
-    // Get all the navigation links
+    
     const navLinks = document.querySelectorAll('nav a');
   
-    // Add click event listeners to each navigation link
     navLinks.forEach(function (link) {
         link.addEventListener('click', function (event) {
             event.preventDefault();
   
-            // Get the target section based on the href attribute of the link
             const targetId = link.getAttribute('href').substr(1);
             const targetSection = document.getElementById(targetId);
   
-            // Hide all sections
             document.querySelectorAll('section').forEach(function (section) {
                 section.style.display = 'none';
             });
   
-            // Show the target section
             targetSection.style.display = 'block';
         });
     });
--- itis3135/scripts/script.js
+++ itis3135/scripts/script.js
@@ -1,4 +1,3 @@
-// script.js
 document.querySelectorAll('.gallery-item').forEach(item => {
     item.addEventListener('click', () => {
         const imgSrc = item.querySelector('img').getAttribute('src');
