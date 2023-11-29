a2c3bdefbd9d1f0bd35a20d37581f6359d0395c6
Ialzouby <85916427+Ialzouby@users.noreply.github.com>
2023-11-29 18:21:13 -0500

    Midterm

--- itis3135/scripts/page.js
+++ itis3135/scripts/page.js
@@ -1,31 +1,26 @@
-// Wait for the document to fully load before executing the script
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
+    // Show the home section when the page loads
+    document.getElementById('home').style.display = 'block';
+  
     // Get all the navigation links
-    const navLinks = document.querySelectorAll("nav ul li a");
-
-    // Get all the sections
-    const sections = document.querySelectorAll("section");
-
-    // Show the "home" section by default
-    showSection("home");
-
-    // Add click event listeners to navigation links
-    navLinks.forEach((link) => {
-        link.addEventListener("click", (e) => {
-            e.preventDefault();
-            const targetSectionId = link.getAttribute("href").substring(1);
-            showSection(targetSectionId);
+    const navLinks = document.querySelectorAll('nav ul li a');
+  
+    // Add click event listeners to each navigation link
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+  
+            // Get the target section based on the href attribute of the link
+            const targetId = link.getAttribute('href').substr(1);
+            const targetSection = document.getElementById(targetId);
+  
+            // Hide all sections
+            document.querySelectorAll('section').forEach(function (section) {
+                section.style.display = 'none';
+            });
+  
+            // Show the target section
+            targetSection.style.display = 'block';
         });
     });
-
-    // Function to show a specific section and hide others
-    function showSection(sectionId) {
-        sections.forEach((section) => {
-            if (section.getAttribute("id") === sectionId) {
-                section.style.display = "block";
-            } else {
-                section.style.display = "none";
-            }
-        });
-    }
-});
+  });
